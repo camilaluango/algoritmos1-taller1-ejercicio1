f289b46d6ba8c38c4d1b4e21921c0a81524f77ec
camila <mariacamilaluango@gmail.com>
2024-10-18 22:18:11 -0500

    calcular total

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 let precio = parseInt (prompt ("cual es el precio de la prenda?"))
 let desc = parseInt (prompt ("de cuanto es el descuento que desea agregar?"))
+let res =(precio*desc)/100, total=precio-res
+
